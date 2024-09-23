05df8e5ae1e56e0030f8d4f3ed933fb0dd346546
kawalero <73931905+kawalerooo@users.noreply.github.com>
2024-09-23 10:29:07 +0200

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,5 +1,5 @@
 <?php
-const DB_HOSTNAME = "sql207.epizy.com";
-const DB_USER = "epiz_32991396";
-const DB_PASSWORD = "Y8UqQRCI2s";
-const DB_NAME = "epiz_32991396_z19nr2";
+const DB_HOSTNAME = "";
+const DB_USER = "";
+const DB_PASSWORD = "";
+const DB_NAME = "";
